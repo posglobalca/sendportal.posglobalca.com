5d9f3575990ea43f8a05c8d24e2c0fb08bcf5d20
rafael1delgado <delgadorafael2011@gmail.com>
2021-05-12 17:05:19 -0400

    [add] SendMailCommand

---
+++ app/Console/Commands/SendMailCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SendMailCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Comando para enviar los correos de la cola';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $exitCode = Artisan::call('queue:work', [
+            '--queue'       => 'sendportal-message-dispatch',
+            '--tries'       => 3,
+            '--max-time'    => 60,
+            '--max-jobs'    => 10,
+        ]);
+    }
+}
